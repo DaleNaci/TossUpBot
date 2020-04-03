f58fa7ed91d279be6a4b5fd781af88626f59ae47
DaleNaci <dale.nash01@gmail.com>
2020-04-03 01:59:36 -0400

    Adding Discord Bot

---
+++ Main.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+from mcstatus import MinecraftServer
+
+import asyncio
+
+Client = discord.Client()
+client = commands.Bot(command_prefix = "!")
+
+server = MinecraftServer.lookup("Vextossup.join-mc.net")
+status = server.status()
+
+@client.event
+async def on_ready():
+    print("Bot is ready!")
+
+@client.command()
+async def p(ctx):
+    player_count = status.players.online
+    reply = str(player_count) + " players are currently online."
+
+    await ctx.send(reply)
+
+
+with open("token.txt", "r") as f:
+    lines = f.readlines()
+    token = lines[0].strip()
+
+client.run(token)
